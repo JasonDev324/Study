0fd83f917a2557af3552a10ee55096dcc2d94e39
TanJunDang <TanJunDang324@gmail.com>
2016-09-08 13:07:20 +0800

    完善PreferenceActivity示例

--- app/src/main/java/io/tanjundang/study/test/preference/PreSettingActivity.java
+++ app/src/main/java/io/tanjundang/study/test/preference/PreSettingActivity.java
@@ -1,7 +1,18 @@
 package io.tanjundang.study.test.preference;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.List;
 
@@ -15,9 +26,120 @@ import io.tanjundang.study.R;
 
 public class PreSettingActivity extends PreferenceActivity {
 
+    AppCompatDelegate mDelegate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("设置");
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getDelegate().onPostCreate(savedInstanceState);
+    }
+
+    public ActionBar getSupportActionBar() {
+        return getDelegate().getSupportActionBar();
+    }
+
     @Override
     public void onBuildHeaders(List<Header> target) {
         super.onBuildHeaders(target);
         loadHeadersFromResource(R.xml.preference_headers, target);
     }
+
+
+    AppCompatDelegate getDelegate() {
+        if (mDelegate == null) {
+            mDelegate = AppCompatDelegate.create(this, null);
+        }
+        return mDelegate;
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        //解决在4.4崩溃报错的问题
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+//    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+//        getDelegate().setSupportActionBar(toolbar);
+//    }
+
+//    @Override
+//    public MenuInflater getMenuInflater() {
+//        return getDelegate().getMenuInflater();
+//    }
+
+//    @Override
+//    public void setContentView(@LayoutRes int layoutResID) {
+//        getDelegate().setContentView(layoutResID);
+//    }
+
+//    @Override
+//    public void setContentView(View view) {
+//        getDelegate().setContentView(view);
+//    }
+//
+//    @Override
+//    public void setContentView(View view, ViewGroup.LayoutParams params) {
+//        getDelegate().setContentView(view, params);
+//    }
+
+//    @Override
+//    public void addContentView(View view, ViewGroup.LayoutParams params) {
+//        getDelegate().addContentView(view, params);
+//    }
+//
+//    @Override
+//    protected void onPostResume() {
+//        super.onPostResume();
+//        getDelegate().onPostResume();
+//    }
+//
+//    @Override
+//    protected void onTitleChanged(CharSequence title, int color) {
+//        super.onTitleChanged(title, color);
+//        getDelegate().setTitle(title);
+//    }
+//
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//        getDelegate().onConfigurationChanged(newConfig);
+//    }
+
+
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        getDelegate().onStop();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        getDelegate().onDestroy();
+//    }
+//
+//    public void invalidateOptionsMenu() {
+//        getDelegate().invalidateOptionsMenu();
+//    }
+
+
 }
--- app/src/main/java/io/tanjundang/study/test/preference/UsuallyFragment.java
+++ app/src/main/java/io/tanjundang/study/test/preference/UsuallyFragment.java
@@ -1,7 +1,12 @@
 package io.tanjundang.study.test.preference;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import io.tanjundang.study.R;
 
@@ -16,9 +21,28 @@ import io.tanjundang.study.R;
  */
 public class UsuallyFragment extends PreferenceFragment {
 
+    PreSettingActivity mActivity;
+    PreSettingActivity MM;
+
+    /**
+     * 为毛不执行onAttatch
+     * @param context
+     */
+    @Override
+    public void onAttach(Context context) {
+        if (context instanceof PreSettingActivity) {
+            mActivity = (PreSettingActivity) context;
+        }
+        super.onAttach(context);
+    }
+
+    @SuppressLint("NewApi")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.fragment_usually);
+        mActivity = (PreSettingActivity) getActivity();
+        mActivity.getSupportActionBar().setTitle("常用设置");
     }
+
 }
