ed1c6418560788c04c8487d1b933833a0b2474e6
TanJunDang <TanJunDang324@gmail.com>
2016-09-14 11:30:39 +0800

    新增ActionBar Item

--- app/src/main/java/io/tanjundang/study/test/actionbar/MActionBarActivity.java
+++ app/src/main/java/io/tanjundang/study/test/actionbar/MActionBarActivity.java
@@ -4,8 +4,12 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import io.tanjundang.study.R;
+import io.tanjundang.study.common.tools.Functions;
 
 public class MActionBarActivity extends AppCompatActivity {
 
@@ -18,5 +22,25 @@ public class MActionBarActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_send:
+                Functions.toast("发送");
+                break;
+            case R.id.action_setting:
+                Functions.toast("设置");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
