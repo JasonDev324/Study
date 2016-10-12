9f7906f2860b19184fa99ff605e83c3d1cbb0926
TanJunDang <TanJunDang324@gmail.com>
2016-10-12 18:09:42 +0800

    添加微信分享

--- app/src/main/java/io/tanjundang/study/base/BaseApplication.java
+++ app/src/main/java/io/tanjundang/study/base/BaseApplication.java
@@ -16,13 +16,23 @@ import io.tanjundang.study.common.tools.ImageLoaderTool;
  */
 
 public class BaseApplication extends Application {
+
+    private final String QQ_ID = "1105742570";
+    private final String QQ_KEY = "WbVyNv1fGXenRLoz";
+    private final String WEIXIN_ID = "wx49b2fe6f13b67083";
+    private final String WEIXIN_KEY = "21fbc78e0d7f1990198b37640845b5d3";
+    private final String WEIBO_ID = "3688550464";
+    private final String WEIBO_KEY = "570706d7dd026e57e4bffea3e7c94938";
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         ImageLoaderTool.initImageLoader(getApplicationContext());
         Functions.init(getApplicationContext());
         UMShareAPI.get(this);
-//        PlatformConfig.setSinaWeibo("3688550464", "570706d7dd026e57e4bffea3e7c94938");
-        PlatformConfig.setQQZone("1105742570", "WbVyNv1fGXenRLoz");
+        PlatformConfig.setSinaWeibo(WEIBO_ID, WEIBO_KEY);
+        PlatformConfig.setQQZone(QQ_ID, QQ_KEY);
+        PlatformConfig.setWeixin(WEIXIN_ID, WEIXIN_KEY);
     }
 }
