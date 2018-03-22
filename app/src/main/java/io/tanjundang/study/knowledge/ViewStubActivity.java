c48bf261c2ba66a0ba920e3c4e7e2154013f81bb
TanJunDang <tanjundang324@gmail.com>
2018-03-22 11:27:06 +0800

    Modify ViewStub DEMO

--- app/src/main/java/io/tanjundang/study/knowledge/ViewStubActivity.java
+++ app/src/main/java/io/tanjundang/study/knowledge/ViewStubActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import io.tanjundang.study.R;
@@ -15,32 +16,41 @@ import io.tanjundang.study.R;
  * @Description: ViewStub
  * 作用：延迟加载View
  * 特性：加载一次后就消失,适用于只加载一次后就不变的布局
- * 参考：http://blog.csdn.net/cx1229/article/details/53505903
+ * 使用参考：http://blog.csdn.net/cx1229/article/details/53505903
+ * 场景参考：http://www.androidchina.net/5550.html
  */
 
 public class ViewStubActivity extends AppCompatActivity {
 
     private ViewStub viewStub;
-    private TextView tvContent;
-    private Button btnMsg;
+    private LinearLayout llRootView;
+    private Button btnModify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stub);
         viewStub = (ViewStub) findViewById(R.id.viewStub);
-        btnMsg = (Button) findViewById(R.id.btnMsg);
+        btnModify = (Button) findViewById(R.id.btnModify);
     }
 
     public void inflate(View v) {
         if (viewStub.getParent() != null) {
+//            第一次点击 初始化布局
             viewStub.inflate();
-            btnMsg.setVisibility(View.GONE);
+            llRootView = (LinearLayout) findViewById(R.id.llRootView);
+            btnModify.setText("收起");
+        } else {
+//            之后的点击就是show hide了
+            if (llRootView.getVisibility() == View.GONE) {
+                llRootView.setVisibility(View.VISIBLE);
+                btnModify.setText("收起");
+            } else {
+                llRootView.setVisibility(View.GONE);
+                btnModify.setText("展开");
+            }
         }
-        tvContent = (TextView) findViewById(R.id.tvContent);
     }
 
-    public void doSomeThing(View v) {
-        tvContent.setText("777");
-    }
+
 }
