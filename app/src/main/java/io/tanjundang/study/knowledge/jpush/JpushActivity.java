1937c75a8f4fa4447772fc2bf2e8242edadce114
TanJunDang <TanJunDang@126.com>
2019-02-27 09:09:46 +0800

    极光推送界面

---
+++ app/src/main/java/io/tanjundang/study/knowledge/jpush/JpushActivity.java
@@ -0,0 +1,20 @@
+package io.tanjundang.study.knowledge.jpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.tanjundang.study.R;
+
+/**
+ * @Author: TanJunDang
+ * @Date: 2019/2/25 17:39
+ * @Description: 极光推送
+ */
+public class JpushActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jpush);
+    }
+}
