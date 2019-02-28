c879812defc1b9f598ca95503526077500c50671
TanJunDang <TanJunDang@126.com>
2019-02-28 14:57:52 +0800

    Camera2自定义相机

--- app/src/main/java/io/tanjundang/study/knowledge/camera/CustomCameraActivity.java
+++ app/src/main/java/io/tanjundang/study/knowledge/camera/CustomCameraActivity.java
@@ -1,35 +1,382 @@
 package io.tanjundang.study.knowledge.camera;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
-import android.view.SurfaceView;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.tanjundang.study.R;
-import io.tanjundang.study.base.BaseActivity;
 
-public class CustomCameraActivity extends BaseActivity {
+/**
+ * @Author: TanJunDang
+ * @Date: 2019/2/28 14:55
+ * @Description: 参考
+ * https://www.cnblogs.com/kingwild/articles/5422329.html以及官方demo
+ * https://github.com/googlesamples/android-Camera2Basic
+ */
+public class CustomCameraActivity extends AppCompatActivity {
 
-    private SurfaceView surfaceView;
+    @BindView(R.id.textureView)
+    AutoFitTextureView textureView;
+    // 摄像头ID（通常0代表后置摄像头，1代表前置摄像头）
+    private String mCameraId = "0";
+    // 定义代表摄像头的成员变量
+    private CameraDevice mCameraDevice;
+    // 预览尺寸
+    private Size mPreviewSize;
+
+    private CaptureRequest.Builder previewRequestBuilder;
+    // 定义用于预览照片的捕获请求
+    private CaptureRequest previewRequest;
+    // 定义CameraCaptureSession成员变量
+    private CameraCaptureSession captureSession;
+    private ImageReader mImageReader;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void initView() {
         setContentView(R.layout.activity_custom_camera);
-        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        ButterKnife.bind(this);
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                //当SurefaceTexture可用的时候，设置相机参数并打开相机
+                setUpCameraOutputs(width, height);
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
     }
 
-    @Override
-    protected void initData() {
-
+    private void setUpCameraOutputs(int width, int height) {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            // 获取指定摄像头的特性
+            CameraCharacteristics characteristics
+                    = manager.getCameraCharacteristics(mCameraId);
+            // 获取摄像头支持的配置属性
+            StreamConfigurationMap map = characteristics.get(
+                    CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            // 获取摄像头支持的最大尺寸
+            Size largest = Collections.max(
+                    Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                    new CompareSizesByArea());
+            // 创建一个ImageReader对象，用于获取摄像头的图像数据
+            mImageReader = ImageReader.newInstance(largest.getWidth(), largest.getHeight(),
+                    ImageFormat.JPEG, 2);
+            mImageReader.setOnImageAvailableListener(
+                    new ImageReader.OnImageAvailableListener() {
+                        // 当照片数据可用时激发该方法
+                        @Override
+                        public void onImageAvailable(ImageReader reader) {
+                            // 获取捕获的照片数据
+                            Image image = reader.acquireNextImage();
+                            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                            byte[] bytes = new byte[buffer.remaining()];
+                            // 使用IO流将照片写入指定文件
+                            File file = new File(getExternalFilesDir(null), "pic.jpg");
+                            buffer.get(bytes);
+                            try (
+                                    FileOutputStream output = new FileOutputStream(file)) {
+                                output.write(bytes);
+                                Toast.makeText(CustomCameraActivity.this, "保存: "
+                                        + file, Toast.LENGTH_SHORT).show();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                image.close();
+                            }
+                        }
+                    }, null);
+            // 获取最佳的预览尺寸
+            mPreviewSize = chooseOptimalSize(map.getOutputSizes(
+                    SurfaceTexture.class), width, height, largest);
+            // 根据选中的预览尺寸来调整预览组件（TextureView）的长宽比
+            int orientation = getResources().getConfiguration().orientation;
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                textureView.setAspectRatio(mPreviewSize.getWidth(), mPreviewSize.
+                        getHeight());
+            } else {
+                textureView.setAspectRatio(mPreviewSize.getHeight(),
+                        mPreviewSize.getWidth());
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.out.println("出现错误。");
+        }
     }
+
+    private void createCameraPreviewSession() {
+        try {
+            SurfaceTexture texture = textureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            // 将textureView的surface作为CaptureRequest.Builder的目标
+            Surface surface = new Surface(texture);
+
+            // 创建作为预览的CaptureRequest.Builder
+            previewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+            previewRequestBuilder.addTarget(surface);
+            // 创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
+            mCameraDevice.createCaptureSession(Arrays.asList(surface
+                    , mImageReader.getSurface()), new CameraCaptureSession.StateCallback() // ③
+                    {
+                        @Override
+                        public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                            // 如果摄像头为null，直接结束方法
+                            if (null == mCameraDevice) {
+                                return;
+                            }
+                            // 当摄像头已经准备好时，开始显示预览
+                            captureSession = cameraCaptureSession;
+                            try {
+                                // 设置自动对焦模式
+                                previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                                // 设置自动曝光模式
+                                previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                                        CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                                // 开始显示相机预览
+                                previewRequest = previewRequestBuilder.build();
+                                // 设置预览时连续捕获图像数据
+                                captureSession.setRepeatingRequest(previewRequest,
+                                        null, null);  // ④
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+                            Toast.makeText(CustomCameraActivity.this, "配置失败！"
+                                    , Toast.LENGTH_SHORT).show();
+                        }
+                    }, null
+            );
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Size chooseOptimalSize(Size[] choices
+            , int width, int height, Size aspectRatio) {
+        // 收集摄像头支持的大过预览Surface的分辨率
+        List<Size> bigEnough = new ArrayList<>();
+        int w = aspectRatio.getWidth();
+        int h = aspectRatio.getHeight();
+        for (Size option : choices) {
+            if (option.getHeight() == option.getWidth() * h / w &&
+                    option.getWidth() >= width && option.getHeight() >= height) {
+                bigEnough.add(option);
+            }
+        }
+        // 如果找到多个预览尺寸，获取其中面积最小的
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSizesByArea());
+        } else {
+            System.out.println("找不到合适的预览尺寸！！！");
+            return choices[0];
+        }
+    }
+
+    // 为Size定义一个比较器Comparator
+    static class CompareSizesByArea implements Comparator<Size> {
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            // 强转为long保证不会发生溢出
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+                    (long) rhs.getWidth() * rhs.getHeight());
+        }
+    }
+
+    private void openCamera() {
+        //获取摄像头的管理者CameraManager
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        //检查权限
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            //打开相机，第一个参数指示打开哪个摄像头，第二个参数stateCallback为相机的状态回调接口，第三个参数用来确定Callback在哪个线程执行，为null的话就在当前线程执行
+            manager.openCamera(mCameraId, stateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startPreview() {
+        SurfaceTexture mSurfaceTexture = textureView.getSurfaceTexture();
+        //设置TextureView的缓冲区大小
+        mSurfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        //获取Surface显示预览数据
+        Surface mSurface = new Surface(mSurfaceTexture);
+        try {
+            //创建CaptureRequestBuilder，TEMPLATE_PREVIEW比表示预览请求
+            previewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            //设置Surface作为预览数据的显示界面
+            previewRequestBuilder.addTarget(mSurface);
+            //创建相机捕获会话，第一个参数是捕获数据的输出Surface列表，第二个参数是CameraCaptureSession的状态回调接口，当它创建好后会回调onConfigured方法，第三个参数用来确定Callback在哪个线程执行，为null的话就在当前线程执行
+            mCameraDevice.createCaptureSession(Arrays.asList(mSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    captureSession = session;
+                    try {
+                        // 设置自动对焦模式
+                        previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                        // 设置自动曝光模式
+                        previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                                CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                        //创建捕获请求
+                        previewRequest = previewRequestBuilder.build();
+
+                        //设置反复捕获数据的请求，这样预览界面就会一直有数据显示
+                        captureSession.setRepeatingRequest(previewRequest, null, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+            //开启预览
+//            startPreview();
+            createCameraPreviewSession();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+
+        }
+    };
 
     public void takePhoto(View v) {
-
+        captureStillPicture();
     }
 
-
+    private void captureStillPicture() {
+        try {
+            if (mCameraDevice == null) {
+                return;
+            }
+            // 创建作为拍照的CaptureRequest.Builder
+            final CaptureRequest.Builder captureRequestBuilder = mCameraDevice
+                    .createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            // 将imageReader的surface作为CaptureRequest.Builder的目标
+            captureRequestBuilder.addTarget(mImageReader.getSurface());
+            // 设置自动对焦模式
+            captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            // 设置自动曝光模式
+            captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                    CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+            // 获取设备方向
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            // 根据设备方向计算设置照片的方向
+            captureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION
+                    , ORIENTATIONS.get(rotation));
+            // 停止连续取景
+            captureSession.stopRepeating();
+            // 捕获静态图像
+            captureSession.capture(captureRequestBuilder.build()
+                    , new CameraCaptureSession.CaptureCallback()  // ⑤
+                    {
+                        // 拍照完成时激发该方法
+                        @Override
+                        public void onCaptureCompleted(CameraCaptureSession session
+                                , CaptureRequest request, TotalCaptureResult result) {
+                            try {
+                                // 重设自动对焦模式
+                                previewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
+                                        CameraMetadata.CONTROL_AF_TRIGGER_CANCEL);
+                                // 设置自动曝光模式
+                                previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                                        CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                                // 打开连续取景模式
+                                captureSession.setRepeatingRequest(previewRequest, null, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
