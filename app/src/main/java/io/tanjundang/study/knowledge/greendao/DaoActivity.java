408e2a88fd4ae8c64e137f7afea162a8e4895277
TanJunDang <TanJunDang@126.com>
2019-08-22 17:19:19 +0800

    删除greendao

--- app/src/main/java/io/tanjundang/study/knowledge/greendao/DaoActivity.java
+++ app/src/main/java/io/tanjundang/study/knowledge/greendao/DaoActivity.java
@@ -1,6 +1,5 @@
 package io.tanjundang.study.knowledge.greendao;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.greenrobot.greendao.query.QueryBuilder;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.tanjundang.study.R;
-import io.tanjundang.study.test.Person;
 
 /**
  * @Author: TanJunDang
@@ -51,32 +46,17 @@ public class DaoActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btnInsert:
-                Person person = new Person(count, "TJD" + count);
-                try {
-                    DaoManager.getInstance(this).getDaoSession().getPersonDao().insert(person);
-                    Snackbar.make(view, "插入数据成功", Snackbar.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    Snackbar.make(view, "插入数据失败，数据已存在", Snackbar.LENGTH_LONG).show();
-                    e.printStackTrace();
-                }
-                count++;
+
                 break;
             case R.id.loadData:
-                tvContent.setText(null);
-                List<Person> personList = DaoManager.getInstance(this).getDaoSession().getPersonDao().loadAll();
-                for (int i = 0; i < personList.size(); i++) {
-                    tvContent.append("name:" + personList.get(i).getName() + "    id:" + personList.get(i).getId() + "\n");
-                }
+
                 break;
             case R.id.btnDelByKey:
-                DaoManager.getInstance(this).getDaoSession().getPersonDao().deleteByKey(count - 1);
-                count--;
                 Snackbar.make(view, "删除数据成功", Snackbar.LENGTH_LONG).show();
                 break;
             case R.id.btnDel:
                 count = 0;
                 tvContent.setText(null);
-                DaoManager.getInstance(this).getDaoSession().getPersonDao().deleteAll();
                 Snackbar.make(view, "删除所有数据", Snackbar.LENGTH_LONG).show();
                 break;
 
